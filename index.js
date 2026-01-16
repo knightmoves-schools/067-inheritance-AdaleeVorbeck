31b3c391ad5bb03cf05ba9e582f57a4d09f2a36e
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-16 18:48:00 +0000

    SLEEP UNTIL THE SUN GOES DOWN

--- index.js
+++ index.js
@@ -23,3 +23,14 @@ class Car{
         this.year = year;
     }
 }
+
+class Agable {
+    constructor (yearEstablished) {
+        
+    }
+}
+
+//should create a class named Agable that takes a year and has an age method that calculates the age (current year - objects year) (use new Date().getFullYear() to get the current year)
+//should inherit from Agable in Company class and pass yearEstablished to Agable in the constructor
+//should inherit from Agable in Person class and pass birthYear to Agable in the constructor
+//should inherit from Agable in Car class and pass year to Agable in the constructor
