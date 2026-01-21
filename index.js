9ed2246e4c5f29f1cab918ba9ab485f9a6ee9b79
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-21 20:00:41 +0000

    LIKE THE KINGS AND QUEENS OF BETTER YESTERDAYS

--- index.js
+++ index.js
@@ -25,8 +25,38 @@ class Car{
 }
 
 class Agable {
-    constructor (yearEstablished) {
-        
+    constructor (year) {
+        this.year = year;
+    }
+    
+    age() {
+        return new Date().getFullYear() - this.year;
+    }
+}
+
+class Company extends Agable {
+    constructor(name, taxId, yearEstablished, taxRate){
+        super(yearEstablished);
+        this.name = name;
+        this.id = taxId;
+        this.taxRate = taxRate;
+    }
+}
+
+class Person extends Agable {
+    constructor(name, ssn, birthYear, taxRate){
+        super(birthYear);
+        this.name = name;
+        this.id = ssn;
+        this.taxRate = taxRate;
+    }
+}
+
+class Car extends Agable {
+    constructor(model, vin, year){
+        super(year);
+        this.name = model;
+        this.id = vin;
     }
 }
 
